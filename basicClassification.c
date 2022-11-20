498b74cef26fa778714efc5944c5132b011b4556
LiorShiboli <lior12sh@gmail.com>
2022-11-20 14:58:22 +0200

    started project

---
+++ basicClassification.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "numclass.h"
+#include <math.h>
+int isPrime(int num){
+    int isprime = 0;
+    for (int i = 0; i < sqrt( (double) num); i++)
+    {
+        
+    }
+    
+}
---
+++ numclass.h
@@ -0,0 +1,7 @@
+#ifndef NUMCLASS
+#define NUMCLASS
+int isArmStrong(int);
+int isPalindrome(int);
+int isPrime(int);
+int isStrong(int);
+#endif
